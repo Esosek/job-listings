095f334d3679cbb41d395c855582609d4f3ca0bc
Ales Zima <ales@cpex.cz>
2024-06-28 11:13:44 +0200

    Added globals.css for Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { League_Spartan } from 'next/font/google';
+import './globals.css';
 import favicon from '@/public/images/favicon.png';
 
 const leagueSpartan = League_Spartan({ subsets: ['latin'] });
