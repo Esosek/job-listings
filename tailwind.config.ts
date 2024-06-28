14378aecdd46060ef71ade74185199f9c94dcbfb
Ales Zima <ales@cpex.cz>
2024-06-28 14:47:32 +0200

    Added colors to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'hsl(180, 29%, 50%)',
+        'cyan-100': 'hsl(180, 52%, 96%)',
+        'cyan-200': 'hsl(180, 31%, 95%)',
+        'cyan-500': 'hsl(180, 8%, 52%)',
+        'cyan-700': 'hsl(180, 14%, 20%)',
+      },
       fontSize: {
         base: '0.9375rem',
       },
