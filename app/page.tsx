cb54d9b128fbdb4b554cb163f19126916800e991
Ales Zima <ales@cpex.cz>
2024-07-01 11:32:17 +0200

    Created Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import JobItem from '@/components/JobItem';
-import styles from './page.module.css';
+
 import jobData from '@/data.json';
 import { FilterContextProvider } from '@/context/FilterContext';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <FilterContextProvider>
-      <header
-        className={`${styles.header} bg-primary h-[156px] w-auto`}
-      ></header>
+      <Header />
       <main className="max-w-5xl px-6 mx-auto">
         <ul>
           {jobData.map((job) => (
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,7 @@
+import styles from './Header.module.css';
+
 export default function Header() {
-  return <header className="bg-primary h-[156px] w-auto object-cover"></header>;
+  return (
+    <header className={`${styles.header} bg-primary h-[156px] w-auto`}></header>
+  );
 }
